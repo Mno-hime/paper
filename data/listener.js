42a8675ede520d303b15cf44e96b2902fac6f1ce
Yegor Timoshenko <mail@yegortimoshenko.com>
2015-07-30 18:32:55 +0000

    Rewrite the addon
    This commit features a better, more readable and more maintainable
    codebase to work with. Now there are three entities: main script
    that initializes the addon, button module that tracks button state
    and contains some glue code to send URLs to Instapaper, and API
    module called `instapaper`.
    
    `listener.js` readability is improved as well.

--- data/listener.js
+++ data/listener.js
@@ -1,35 +1,33 @@
-function listener(event) {
+function emitURL(event) {
   var link = event.target;
-  while (link && link.localName != "a")
+  while (link && link.localName != 'a')
     link = link.parentNode;
 
   if (link) {
-    self.port.emit("click", link.href);
+    self.port.emit('click', link.href);
     event.preventDefault();
   }
 }
 
-var checkModifier = function(modifier) {
-  switch (modifier) {
-    case "S":  return function (e) { return e.shiftKey }
-    case "C":  return function (e) { return e.ctrlKey }
-    case "CS": return function (e) { return e.ctrlKey && e.shiftKey }
-    case "CA": return function (e) { return e.ctrlKey && e.altKey }
-    case "A":  return function (e) { return e.altKey }
-    case "AS": return function (e) { return e.altKey && e.shiftKey }
-    case "AC": return function (e) { return e.altKey && e.ctrlKey }
-    case "M":  return function (e) { return e.metaKey }
-    case "MS": return function (e) { return e.metaKey && e.shiftKey }
-    case "MC": return function (e) { return e.metaKey && e.ctrlKey }
-  }
-}(self.options.modifier);
+var isModifierPressed = {
+  'S':  function(e) { return e.shiftKey },
+  'A':  function(e) { return e.altKey },
+  'AC': function(e) { return e.altKey && e.ctrlKey },
+  'AS': function(e) { return e.altKey && e.shiftKey },
+  'C':  function(e) { return e.ctrlKey },
+  'CA': function(e) { return e.ctrlKey && e.altKey },
+  'CS': function(e) { return e.ctrlKey && e.shiftKey },
+  'M':  function(e) { return e.metaKey },
+  'MC': function(e) { return e.metaKey && e.ctrlKey },
+  'MS': function(e) { return e.metaKey && e.shiftKey }
+}[self.options.modifier];
 
-window.addEventListener("keydown", function(event) {
-  if (checkModifier(event))
-    window.addEventListener("click", listener, false);
+window.addEventListener('keydown', function(event) {
+  if (isModifierPressed(event))
+    window.addEventListener('click', emitURL, false);
 });
 
-window.addEventListener("keyup", function(event) {
-  if (!checkModifier(event))
-    window.removeEventListener("click", listener, false);
+window.addEventListener('keyup', function(event) {
+  if (!isModifierPressed(event))
+    window.removeEventListener('click', emitURL, false);
 });
