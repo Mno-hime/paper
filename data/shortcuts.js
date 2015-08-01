e7dfc6d0669b937179861ee96a030ec55ecbc1d5
Yegor Timoshenko <mail@yegortimoshenko.com>
2015-08-01 20:12:54 +0000

    Provide shortcuts for reading mode (see #13)

---
+++ data/shortcuts.js
@@ -0,0 +1,21 @@
+window.addEventListener('keydown', function(event) {
+  event.preventDefault();
+
+  switch (event.keyCode) { 
+    case 74: // j
+      window.scrollBy(0, 100);
+      break;
+    case 75: // k
+      window.scrollBy(0, -100);
+      break;
+    case 65: // a
+      document.getElementsByClassName('js_archive_single')[0].click();
+      break;
+    case 76: // l
+      document.getElementsByClassName('js_star_toggle')[0].click();
+      break;
+    case 68: // d
+      document.getElementsByClassName('js_delete_single')[0].click();
+      break;
+  }
+});
