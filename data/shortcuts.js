e77b4d4c5d55afa9bed28967822c3cb5260d9732
Yegor Timoshenko <mail@yegortimoshenko.com>
2015-08-04 14:11:55 +0000

    Disable shortcuts when adding notes, fixes #20

--- data/shortcuts.js
+++ data/shortcuts.js
@@ -1,21 +1,23 @@
 let step = self.options.step;
 
 window.addEventListener('keydown', function(event) {
-  switch (event.keyCode) { 
-    case 74: // j
-      window.scrollByLines(step);
-      break;
-    case 75: // k
-      window.scrollByLines(-step);
-      break;
-    case 65: // a
-      document.getElementsByClassName('js_archive_single')[0].click();
-      break;
-    case 76: // l
-      document.getElementsByClassName('js_star_toggle')[0].click();
-      break;
-    case 68: // d
-      document.getElementsByClassName('js_delete_single')[0].click();
-      break;
+  if (event.target.tagName.toLowerCase() !== 'textarea') {
+    switch (event.keyCode) { 
+      case 74: // j
+        window.scrollByLines(step);
+        break;
+      case 75: // k
+        window.scrollByLines(-step);
+        break;
+      case 65: // a
+        document.getElementsByClassName('js_archive_single')[0].click();
+        break;
+      case 76: // l
+        document.getElementsByClassName('js_star_toggle')[0].click();
+        break;
+      case 68: // d
+        document.getElementsByClassName('js_delete_single')[0].click();
+        break;
+    }
   }
 });
